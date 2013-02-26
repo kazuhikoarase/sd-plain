e976ba16ab62329bf648ec4e93bf4bddd4e80557
kazuhiko arase <kazuhiko.arase@gmail.com>
2013-02-26 13:38:38 +0900

    commit

--- src/java/com/example/logic/CmdX02.sql
+++ src/java/com/example/logic/CmdX02.sql
@@ -16,11 +16,11 @@ from
 TX0001 T1,
 TX0002 T2
 
--- 絞り込み条件
 where T1.COL1 = T2.COL1
 
 and T2.COL1=${param.col1}
 
+-- 絞り込み条件
 --! // col2 が設定されている場合、条件追加
 --! if (param.col2 != null) {
 	 and T2.COL2=${param.col2}
